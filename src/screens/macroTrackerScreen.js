cbfa1e2f5aa06bef4415b48d89fbefdefdc5dfa3
NaDeans <nadeans@outlook.com>
2026-02-26 16:10:41 +1100

    github push/pull test

--- src/screens/macroTrackerScreen.js
+++ src/screens/macroTrackerScreen.js
@@ -4,7 +4,7 @@ import { View, Text} from "react-native";
 export default function MacroTrackerScreen() {
   return (
     <View>
-      <Text>Macro Tracker Screen</Text>
+      <Text>Macro Tracker Screen, this is a test to see if i can edit the code from the other pc</Text>
     </View>
   );
 }
